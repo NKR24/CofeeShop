d4930b845840cb462fe1bedcfb0ef7e7ffe2d9c5
Nikita Khvorostin <khvorostinn@gmail.com>
2024-02-11 19:30:44 +0300

    a revert to 4380cd1e

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar/Navbar.tsx"
 import MainPage from "../components/MainPage/MainPage.tsx"
 import CoffeeStories from "../components/CoffeeStories/CoffeeStories.tsx"
-import CoffeeProduct from "../components/CoffeeProduct/CoffeeProduct.tsx"
+
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
       <Navbar />
       <MainPage />
       <CoffeeStories />
-      <CoffeeProduct />
+      {/*<CoffeeProduct />*/}
     </>
   )
 }
